3a9b87fa5e657a9e4ddd139f7972a60feae89045
Patrick Melo <patrickabimael@yahoo.com.br>
2023-04-19 18:22:48 -0300

    cria conversor de C2 para binário

--- conversores.ts
+++ conversores.ts
@@ -1,4 +1,7 @@
-export const BinarioParaSM = function(numeroBinario: string) {
+import { inverteBits } from "./funcoes-ajudadoras.ts"
+import { somarBinariosPositivos } from "./soma.ts"
+
+export const SMParaBinario = function(numeroBinario: string) {
   let numeroEmSM = ""
   if (numeroBinario[0] === "1") numeroEmSM = "-"
 
@@ -10,4 +13,34 @@ export const BinarioParaSM = function(numeroBinario: string) {
   for (let i = index; i < numeroBinario.length; i++) numeroEmSM += numeroBinario[i]
 
   return numeroEmSM
+}
+
+export const C2paraBinario = function (numeroBinario: string) {
+  if (numeroBinario[0] === "0") return SMParaBinario(numeroBinario)
+  const representacaoPositiva = somarBinariosPositivos(inverteBits(numeroBinario), "1").split('')
+
+  while (representacaoPositiva[0] === "0") representacaoPositiva.shift()
+  representacaoPositiva.unshift("-")
+  return representacaoPositiva.join('')
+}
+
+export const SMparaDec = function (numeroEmSM: string) {
+  const numeroBinario = SMParaBinario(numeroEmSM)
+  let numeroEmDecimal = 0
+  let potenciaDeDois = 1
+
+  for (let index = numeroBinario.length - 1; index > -1; index--) {
+    if (numeroBinario[index] === "1") numeroEmDecimal += potenciaDeDois
+    potenciaDeDois *= 2
+  }
+
+  if (numeroEmDecimal !== 0 && numeroBinario[0] === "-") return -numeroEmDecimal
+  return numeroEmDecimal
+}
+
+export const C2paraDec = function (numeroEmC2: string) {
+  if (numeroEmC2[0] === "0") return SMparaDec(numeroEmC2)
+
+  const representacaoPositiva = somarBinariosPositivos(inverteBits(numeroEmC2), "1")
+  return "-" + SMparaDec(representacaoPositiva)
 }
