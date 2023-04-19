1ac902209b54039240723c0982c0fb62ef7ed020
Patrick Melo <patrickabimael@yahoo.com.br>
2023-04-19 14:21:30 -0300

    cria função de binário para SM

--- conversores.ts
+++ conversores.ts
@@ -1,1 +1,13 @@
-export const C2paraBinario = ""
+export const BinarioParaSM = function(numeroBinario: string) {
+  let numeroEmSM = ""
+  if (numeroBinario[0] === "1") numeroEmSM = "-"
+
+  let index = 1
+  // Esse loop for serve para achar onde o número começa e descartar os zeros.
+  for (index; index < numeroBinario.length; index++) if (numeroBinario[index] === "1") break
+
+  // Esse vai pegar os bits de onde o número começa até o final da string.
+  for (let i = index; i < numeroBinario.length; i++) numeroEmSM += numeroBinario[i]
+
+  return numeroEmSM
+}
